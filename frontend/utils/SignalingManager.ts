b75e358e6d1d649043f591aed56677694fcb09cd
Ayush Acharjya <ayushacharjya@gmail.com>
2024-09-08 20:53:07 +0100

    fix docker issue of ws url + remove console logs

--- frontend/utils/SignalingManager.ts
+++ frontend/utils/SignalingManager.ts
@@ -1,5 +1,3 @@
-const WS_BASE_URL = process.env.NEXT_PUBLIC_WS_BASE_URL as string;
-
 export class SignalingManager {
   private ws: WebSocket;
   private static instance: SignalingManager;
@@ -8,8 +6,12 @@ export class SignalingManager {
   private onMessageCallback: ((message: string) => void) | null = null; // Callback for received messages
 
   private constructor() {
-    console.log("instance created");
-    this.ws = new WebSocket(WS_BASE_URL);
+    const wsUrl = process.env.NEXT_PUBLIC_WS_URL;
+    // console.log("wsUrl", wsUrl);
+    if (!wsUrl) {
+      throw new Error("WebSocket URL is not defined");
+    }
+    this.ws = new WebSocket(wsUrl);
     this.bufferedMessages = [];
     this.init();
   }
@@ -34,14 +36,14 @@ export class SignalingManager {
     // Listen for incoming messages
     this.ws.onmessage = (event) => {
       const message = event.data;
-      console.log("Message received from server:", message);
+      // console.log("Message received from server:", message);
       if (this.onMessageCallback) {
         this.onMessageCallback(message);
       }
     };
 
     this.ws.onclose = () => {
-      console.log("WebSocket connection closed.");
+      // console.log("WebSocket connection closed.");
       this.initialized = false;
     };
 
@@ -49,19 +51,19 @@ export class SignalingManager {
       console.error("WebSocket error:", error);
     };
 
-    console.log("WebSocket initialized");
+    // console.log("WebSocket initialized");
   }
 
   sendMessage(message: any) {
-    console.log("sending message");
+    // console.log("sending message");
     const messageToSend = message;
     if (!this.initialized) {
       this.bufferedMessages.push(messageToSend);
       return;
     }
-    console.log("after if");
+    // console.log("after if");
     this.ws.send(messageToSend);
-    console.log("msg sent");
+    // console.log("msg sent");
   }
 
   setOnMessageCallback(callback: (message: string) => void) {
